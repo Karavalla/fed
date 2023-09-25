1e81a726a4f042976ab9a349ff44eee90b8ea8dc
Karavalla <123866143+Karavalla@users.noreply.github.com>
2023-09-25 13:47:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+
+
+import DigitalClockDemo from './DigitalClockDemo';
+function App(){
+    return(
+        <div>
+            <DigitalClockDemo/>
+        </div>
+    );
+}
+export default App;
